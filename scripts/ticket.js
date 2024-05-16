00b05bd9e02a350671e7270134cdad3c1546e985
Thu20112001 <ThuDuong.Nguyen@edu.sait.ca>
2024-05-16 14:02:22 -0600

    upload

---
+++ scripts/ticket.js
@@ -0,0 +1,24 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.js-modal')
+const modalContainer = document.querySelector('.js-modal-container')
+const modalClose = document.querySelector('.js-modal-close')
+
+function showBuyTickets() {
+    modal.classList.add('open')
+}
+
+function hideBuyTickets() {
+    modal.classList.remove('open')
+}
+
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showBuyTickets)
+}
+
+modalClose.addEventListener('click', hideBuyTickets)
+
+modal.addEventListener('click', hideBuyTickets)
+
+modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation();
+})
